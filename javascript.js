347aaad5d6f19f0c40a24b057006da1dc8f5dc8e
Allan C <59120947+AllanChen-eng@users.noreply.github.com>
2025-04-26 19:41:33 -0700

    Formatted code + add text for draw scenario
    Added statements in the case of a draw between
    player and computer. Also formated code.

--- javascript.js
+++ javascript.js
@@ -1,4 +1,3 @@
-
 let humanScore = 0;
 let computerScore = 0;
 let roundsToPlay = 5;
@@ -13,7 +12,7 @@ function getComputerChoice() {
 
 function getHumanChoice() {
   let player = prompt("Rock, Paper, or Scissors?");
- if(player != null) player.toLowerCase();
+  if (player != null) player.toLowerCase();
   while (
     player != "rock" &&
     player != "paper" &&
@@ -24,54 +23,62 @@ function getHumanChoice() {
   }
   return player;
 }
-function playGame(){
-    humanScore = 0;
-    computerScore = 0;
-    for(let x = 0;x < roundsToPlay; x++){
-        console.log(playRound(getHumanChoice(),getComputerChoice()));
-    }
-    console.log(calculateScore(humanScore,computerScore,roundsToPlay));
+function playGame() {
+  humanScore = 0;
+  computerScore = 0;
+  for (let x = 0; x < roundsToPlay; x++) {
+    console.log(playRound(getHumanChoice(), getComputerChoice()));
+  }
+  console.log(calculateScore(humanScore, computerScore, roundsToPlay));
 }
 function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
-    computerChoice = computerChoice.toLowerCase();
-    if(humanChoice == computerChoice) return "Draw! No Score Change!";
-   return calculateWinner(humanChoice, computerChoice);
-  }
-function calculateWinner(humanChoice, computerChoice){
-    let winMessage = "You Win! " + humanChoice + " beats " + computerChoice;
-    let loseMessage = "You Lose! " + computerChoice + " beats " + humanChoice;
-    if(humanChoice == "rock"){
-        if(computerChoice == "scissors"){
-            humanScore++;
-            return winMessage;
-        }
-        if(computerChoice == "paper"){
-            computerScore++;
-            return loseMessage;
-        }
-    }else if(humanChoice == "paper"){
-        if(computerChoice == "rock"){
-            humanScore++;
-            return winMessage;
-        }
-        if(computerChoice == "scissors"){
-            computerScore++
-            return loseMessage;
-        }
-    } else if(humanChoice == "scissors"){
-        if(computerChoice =="rock"){
-            computerScore++;
-            return loseMessage;
-        }
-        if(computerChoice == "paper"){
-            humanScore++;
-            return winMessage;
-        }
-    }
+  humanChoice = humanChoice.toLowerCase();
+  computerChoice = computerChoice.toLowerCase();
+  if (humanChoice == computerChoice) return "Draw! No Score Change!";
+  return calculateWinner(humanChoice, computerChoice);
 }
-function calculateScore(humanScore,ComputerScore,roundsToPlay){
-    if(humanScore > computerScore) return "You won with a score of " + humanScore + " out of " + roundsToPlay;
-    else return "You lost! The computer beat you by " + (ComputerScore - humanScore) + " points!";
+function calculateWinner(humanChoice, computerChoice) {
+  let winMessage = "You Win! " + humanChoice + " beats " + computerChoice;
+  let loseMessage = "You Lose! " + computerChoice + " beats " + humanChoice;
+  if (humanChoice == "rock") {
+    if (computerChoice == "scissors") {
+      humanScore++;
+      return winMessage;
+    }
+    if (computerChoice == "paper") {
+      computerScore++;
+      return loseMessage;
+    }
+  } else if (humanChoice == "paper") {
+    if (computerChoice == "rock") {
+      humanScore++;
+      return winMessage;
+    }
+    if (computerChoice == "scissors") {
+      computerScore++;
+      return loseMessage;
+    }
+  } else if (humanChoice == "scissors") {
+    if (computerChoice == "rock") {
+      computerScore++;
+      return loseMessage;
+    }
+    if (computerChoice == "paper") {
+      humanScore++;
+      return winMessage;
+    }
+  }
+}
+function calculateScore(humanScore, ComputerScore, roundsToPlay) {
+  if (humanScore == computerScore)
+    return "It's a draw!" + "both players scored " + humanScore + " points!";
+  if (humanScore > computerScore)
+    return "You won with a score of " + humanScore + " out of " + roundsToPlay;
+  else
+    return (
+      "You lost! The computer beat you by " +
+      (ComputerScore - humanScore) +
+      " points!"
+    );
 }
 playGame();
